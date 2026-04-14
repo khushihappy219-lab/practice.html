5c26f0ce60ba6b013222dea7a66a6094071e6a54
khushi malviya <khushihappy219@gmail.com>
2026-04-14 16:00:06 +0530

    message

---
+++ object.js
@@ -0,0 +1,15 @@
+let spreadoperator=()=>
+    {
+let object1={
+ Name:"khushi",
+ class:12,
+}
+let object2={
+    Myname:"priya",
+    myclass:11,
+}
+let combinedobject={...object1,...object2};
+
+console.log(combinedobject);
+}
+spreadoperator()
---
+++ reversearray.js
@@ -0,0 +1,10 @@
+// let array=[2, 4, 6, 8 ,10];
+// array.reverse();
+// console.log(array);
+
+let array=[1,2,3,4,5,6]
+let reversedarray=[];
+for (let i= array.length-1;i>=0;i--){
+    reversedarray.push(array[i])
+}
+console.log(reversedarray)
